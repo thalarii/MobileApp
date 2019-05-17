d7900c690fffad8e9bd17d589347979c7ed31611
Yidnekworku1 <49309971+Yidnekworku1@users.noreply.github.com>
2019-05-17 07:30:06 -0400

    Reciept added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <ToolBar></ToolBar>
+    <div style="padding:10px;background-color: #1976d2;">
+      <ServiceCards/>
+    </div>
+    <v-list two-line>
+      <v-btn flat style="float:right;margin-top" @click="clearHistory">clear all</v-btn>
+      <v-subheader>History</v-subheader>
+      <div v-for="(item, index) in allHistories" :key="index">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img :src="item.url_logo">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-html="item.name"></v-list-tile-title>
+            <v-list-tile-sub-title v-html="item.purchased_with"></v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn flat>
+              <div
+                style="max-width:80px;overflow:hidden;"
+                color="grey lighten-1"
+              >- {{item.price}}.00</div>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-divider inset></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+import ServiceCards from "./ServiceCards.vue";
+import ToolBar from "./ToolBar.vue";
+export default {
+  computed: mapGetters(["allHistories"]),
+  methods: mapActions(["clearHistory"]),
+  data: () => ({
+    drawer: null,
+    itemss: [
+      { title: "Home", icon: "dashboard" },
+      { title: "About", icon: "question_answer" }
+    ],
+    items: [
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+        title: "Ice Cream",
+        subtitle: "CBE Birr"
+      },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        title: "Samsung Galaxy J9",
+        subtitle: "CBE Birr"
+      },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+        title: "Thoshiba G 850",
+        subtitle: "Amole"
+      },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+        title: "Houndi v3",
+        subtitle: "Ethio Telecom"
+      }
+    ]
+  }),
+  name: "HelloWorld",
+  components: {
+    ServiceCards,
+    ToolBar
+  }
+};
+</script>
+
+<style>
+* {
+  font-family: "roboto mono";
+}
+.v-avatar img,
+.v-avatar .v-icon,
+.v-avatar .v-image {
+  border-radius: 10px;
+  /* color:darkmagenta */
+}
+</style>
---
+++ src/components/Reciept.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="text-xs-center">
+    <v-bottom-sheet v-model="recieptSheet" style="border-top-left-radius: 10px;">
+      <!-- <template v-slot:activator>
+        <v-btn color="purple" dark>Click me</v-btn>
+      </template>-->
+      <v-card>
+        <v-toolbar flat>
+          <v-list style="background-color: #e3e3e3;
+  border-bottom: 1px solid #a9a9a9;">
+            <v-list-tile>
+              Recipt
+              <!-- <v-list-tile-title class="title">Application</v-list-tile-title> -->
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <div>
+          <v-card color="cyn darken-2" class="hite--text" style="box-shadow:unset;">
+            <v-layout>
+              <v-flex xs3 style="padding-top:22.5px;margin-left:20px;">
+                <v-img
+                  :src="getAccount.owners_logo_link"
+                  height="80px"
+                  contain
+                  style="border-radius:10px"
+                ></v-img>
+              </v-flex>
+              <v-flex xs7>
+                <v-card-title primary-title>
+                  <div>
+                    <div class="headline">{{getAccount.owners_name}}</div>
+                    <div>{{getAccount.desc}}</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+            <v-divider light></v-divider>
+          </v-card>
+        </div>
+        <div style="padding:10px;">
+          <v-data-table
+            :headers="headers"
+            :items="getCheckOutOrderItems"
+            class="elevaton-1"
+            hide-actions
+          >
+            <template v-slot:items="props">
+              <!-- <tr> -->
+              <td>{{ props.item.item_name }}</td>
+              <td class="text-xs-right">{{ props.item.item_quantity }}</td>
+              <td class="text-xs-right">{{ props.item.item_price }}</td>
+              <!-- </tr> -->
+            </template>
+          </v-data-table>
+          <!-- {{getCheckOutOrderItems}} -->
+        </div>
+        <br>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="purple"
+          dark
+          @click="recieptToServiceSelecter"
+          style="margin-right:200px"
+        >Continue</v-btn>
+      </v-card>
+    </v-bottom-sheet>
+  </div>
+</template>
+<script>
+import { mapState, mapActions, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["recieptSheet"]),
+    ...mapGetters(["getCheckOutOrderItems", "getAccount"])
+  },
+  methods: {
+    ...mapActions(["recieptToServiceSelecter"])
+  },
+  created: () => {
+    console.log("hello");
+  },
+  data: () => ({
+    // sheet: false,
+    headers: [
+      {
+        text: "Item Name ",
+        value: "name"
+      },
+      { text: "Quantity", value: "calories" },
+      { text: "Price (Birr)", value: "fat" }
+    ],
+    checkOutOrders: [
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+        fat: 6.0
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+        fat: 9.0
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+        fat: 16.0
+      }
+    ],
+    tiles: [
+      { img: "keep.png", title: "Keep" },
+      { img: "inbox.png", title: "Inbox" },
+      { img: "hangouts.png", title: "Hangouts" },
+      { img: "messenger.png", title: "Messenger" },
+      { img: "google.png", title: "Google+" }
+    ]
+  })
+};
+</script>
+<style scoped>
+.div.v-list.theme--light {
+  background-color: #e3e3e3;
+  border-bottom: 1px solid #a9a9a9;
+  border-top-left-radius: 10px;
+}
+</style>
