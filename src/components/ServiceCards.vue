eab27e69cfe7eb838bf8a515279806ef61b7928f
abiyabiy36 <49784864+abiyabiy36@users.noreply.github.com>
2019-05-17 07:26:33 -0400

    i have added toolbar and serviceCard

---
+++ src/components/ServiceCards.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-layout style="overflow:auto;">
+    <!-- :style="[service.activated ? {'color:blue'}:{'color:yellow'}]" -->
+
+    <v-flex xs4 sm6 offset-sm2 style="display:flex;">
+      <v-card
+        v-for="(service,index) in allAvaliableServices"
+        :key="index"
+        class="service-card"
+        style="width:120px;margin-right:15px;border-radius:7.5px;border-color:orange;"
+        :class="service.activated"
+        @click="activateService(index)"
+      >
+        <v-container
+          fill-height
+          fluid
+          style="height:75px;"
+          ,
+          :style="{'background-color': service.color}"
+          class="service-card-top"
+        >
+          <v-layout fill-height>
+            <v-flex xs12 align-end flexbox>
+              <span class="headlin" style="color:white;">{{service.name}}</span>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-card-title>
+          <div style="width:100px">
+            <span>USSD Service</span>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "ServiceCards",
+  data: () => ({
+    orange: "orange"
+  }),
+  computed: mapGetters(["allAvaliableServices"]),
+  methods: mapActions(["getCheckOutOrder", "activateService"])
+};
+</script>
+
+<style>
+.false {
+  /* background-color: orange; */
+  opacity: 0.2;
+}
+.service-card {
+  border-bottom: 2px solid green;
+  height: 125px;
+}
+.service-card-top {
+  color: white;
+  background-color: brown;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+}
+</style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-toolbar color="primary" dark style="box-shadow:unset;">
+    <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x>
+      <template v-slot:activator="{ on }">
+        <!-- <v-btn
+          color="indigo"
+          dark
+          v-on="on"
+        >
+          Menu as Popover
+        </v-btn>-->
+        <v-toolbar-side-icon v-on="on"></v-toolbar-side-icon>
+      </template>
+
+      <v-card>
+        <v-list>
+          <v-list-tile avatar>
+            <!-- <v-list-tile-avatar>
+                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+            </v-list-tile-avatar>-->
+
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+              <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <!-- <v-list-tile-action>
+                <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
+                  <v-icon>favorite</v-icon>
+                </v-btn>
+            </v-list-tile-action>-->
+          </v-list-tile>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-switch v-model="message" color="purple"></v-switch>
+            </v-list-tile-action>
+            <v-list-tile-title>Enable messages</v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-switch v-model="hints" color="purple"></v-switch>
+            </v-list-tile-action>
+            <v-list-tile-title>Enable hints</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn flat @click="menu = false">Cancel</v-btn>
+          <v-btn color="primary" flat @click="menu = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+    <!-- <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon> -->
+    <v-toolbar-title class="white--text">Taleri</v-toolbar-title>
+    <!-- <template v-slot:extension>
+    hello-->
+    <!-- <v-toolbar-title class="white--text">Taleri</v-toolbar-title> -->
+    <!-- </template> -->
+
+    <v-spacer></v-spacer>
+
+    <!-- <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>-->
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "ToolBar",
+  data: () => {
+    return {
+      fav: true,
+      menu: false,
+      message: false,
+      hints: true
+    };
+  }
+};
+</script>
+
+<style>
+</style>
